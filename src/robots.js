ecd8418d064ae124dc115d035f37fb945449d0c0
collinswesley <aihiegbecollins@gmail.com>
2020-04-03 22:13:15 +0100

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@pril.biz'
+    },
+    {
+        id: 2,
+        name: 'Ernin Howell',
+        username: 'Antonette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Ibhale Eddy',
+        username: 'Momen',
+        email: 'eddy@myguy.net'
+    },
+    {
+        id: 4,
+        name: 'Matt Churchill',
+        username: 'Okigho',
+        email: 'otoba.chief@oconnect.org'
+    },
+    {
+        id: 5,
+        name: 'Colly Wesley',
+        username: 'Cobons',
+        email: 'lucio_hettiger@inie.ca'
+    },
+    {
+        id: 6,
+        name: 'Ikues Val',
+        username: 'Bobbytino',
+        email: 'billy_duch@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Divine Lucio',
+        username: 'Playboy',
+        email: 'tella.hoeger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Ivie Bliss',
+        username: 'Babygirl',
+        email: 'iviebest@resmond.me'
+    },
+    {
+        id: 9,
+        name: 'Faith Bubu',
+        username: 'Mummyoo',
+        email: 'mybaby_tiano@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Mama Dmama',
+        username: 'Mamayopie',
+        email: 'mama.all@karina.biz'
+    },
+]
